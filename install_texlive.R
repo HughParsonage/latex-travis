58408711955b42cc8e1288de5b9fb4bb1eb7010a
HughParsonage <hugh.parsonage@gmail.com>
2018-02-10 18:29:45 +1100

    Quiet package installation

--- install_texlive.R
+++ install_texlive.R
@@ -1,9 +1,9 @@
 
 if (!requireNamespace("tinytex", quietly = TRUE)) {
 	if (!requireNamespace("devtools", quietly = TRUE)) {
-		install.packages("devtools", repos = "https://cran.rstudio.com/")
+		install.packages("devtools", repos = "https://cran.rstudio.com/", quiet = TRUE)
 	}
-  devtools::install_github(c('yihui/tinytex', 'rstudio/rmarkdown'))
+  devtools::install_github(c('yihui/tinytex', 'rstudio/rmarkdown'), quiet = TRUE)
 	tinytex::install_tinytex()
 }
 
