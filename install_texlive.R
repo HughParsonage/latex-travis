db289027fe4d9178a80c7743d4c4ba9e7d2e328a
HughParsonage <hugh.parsonage@gmail.com>
2018-02-10 13:59:00 +1100

    Ensure devtools installed

--- install_texlive.R
+++ install_texlive.R
@@ -1,9 +1,12 @@
 
 if (!requireNamespace("tinytex", quietly = TRUE)) {
+	if (!requireNamespace("devtools", quietly = TRUE)) {
+		install.packages("devtools", repos = "https://cran.rstudio.com/")
+	}
   devtools::install_github(c('yihui/tinytex', 'rstudio/rmarkdown'))
-	
+	tinytex::install_tinytex()
 }
 
-tinytex::install_tinytex()
+
 
 
